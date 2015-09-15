87ee3281f47d67557666fad49c0ce1d2adb43cb7
priviere <pierre@semencespaysannes.org>
2015-09-15 11:38:44 +0200

    return MCMC only for parameters that converge update model2.presence.abscence.matrix

--- R/predict.the.past.R
+++ R/predict.the.past.R
@@ -39,7 +39,7 @@ predict.the.past = function(
     
   # 3. Get the estimation based on MCMC outputs ----------
   
-  w = out.analyse.outputs$model.presence.abscence.matrix
+  w = out.analyse.outputs$model2.presence.abscence.matrix
   if(is.null(w)) { stop(paste(mess, "Argument analysis in analyse.outputs must be NULL or \"experimental.design\".")) }
   
   pb <- txtProgressBar(min = 0, max = ncol(w), style = 3)
