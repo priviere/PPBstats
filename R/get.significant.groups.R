31dd86e40369829a9f4852a81b811897dad566e6
gaelleVF <gaelle.van.frank@gmail.com>
2016-01-18 15:35:40 +0100

    Correction orthographe

--- R/get.significant.groups.R
+++ R/get.significant.groups.R
@@ -4,7 +4,7 @@
 #' @description
 #' \code{get.significant.groups} gets significant groups of differences for a set of parameters based on MCMC outputs
 #' 
-#' @param Mpvalue Square matrix with the probability to have a common distribution for each pair of parameter. It comes from \code{comp.parameters}
+#' @param Mpvalue Square matrix with the probability of having a common distribution for each pair of parameter. It comes from \code{comp.parameters}
 #'
 #' @param MCMC MCMC outputs from \code{analysis.outputs}. It is a data frame.
 #'  
