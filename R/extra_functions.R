d3425ce298cbd5d85cc70124d97137f690ab20d0
priviere <pierre@semencespaysannes.org>
2016-10-13 14:49:53 +0200

    add new fonction

---
+++ R/extra_functions.R
@@ -0,0 +1,16 @@
+# 0. help -----------------------------------------------------------------
+#' Some functions used in several functions of PPBstats
+#' 
+#' @name extra_functions
+#' 
+#' @description
+#' This file group several functions used in several functions of PPBstats:
+#' \itemize{
+#' 	\item describe_data
+#' }
+#' 
+#' @author Pierre Riviere
+#' 
+
+# describe_data
+
