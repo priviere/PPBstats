274e13b0a2bbed97e22594a29fbb693ce3d3ebbf
priviere <pierre@semencespaysannes.org>
2016-10-17 15:16:03 +0200

    add new fonction

---
+++ R/plan_experiment.R
@@ -0,0 +1,38 @@
+# 0. help ----------
+#' Provides experimental plan for several situations
+#'
+#' @description
+#' \code{plan_experiment} provides experimental plan for several situations
+#' 
+#' @param expe.type The type of experiment to settle "satellite-farm", "regional-farm", "row-columns", "alpha-design", "fully-repicated"
+#' 
+#' @param nb.entries Number of entries
+#' 
+#' @param nb.controls Number of controls. This is useful only for expe.type "satellite-farm" and "regional-farm"
+#' 
+#' @param nb.blocks Number of blocks
+#' 
+#' @param nb.cols Number of columns
+#' 
+#' @param nb.rows Number of rows
+#'
+#' @return 
+#' The function returns a list with
+#' \itemize{
+#'  \item A data frame with X, Y and block
+#'  \item A picture of the experimental plan, which is also exported to csv files
+#'  }
+#' 
+#' @author Pierre Riviere
+#' 
+plan_experiment = function(
+  expe.type,
+  nb.entries,
+  nb.controls
+)
+  # let's go !!! ----------
+  {
+    # 1. Error message ----------  
+
+    return(OUT)
+    }
