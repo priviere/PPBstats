20901502bb48996f16d9ae061d211a0923f0f20c
priviere <pierre@semencespaysannes.org>
2016-11-28 12:34:15 +0100

    add get_biplot

--- R/extra_functions.R
+++ R/extra_functions.R
@@ -12,7 +12,7 @@
 #' @author Pierre Riviere
 #' 
 
-# describe_data
+# Function use in describe_data.R
 check_data_vec_variables = function(data, vec_variables){
   mess = "The following column are compulsory : c(\"year\", \"germplasm\", \"location\", \"block\", \"X\", \"Y\"."
   if(!is.element("year", colnames(data))) { stop(mess) }
@@ -25,7 +25,7 @@ check_data_vec_variables = function(data, vec_variables){
   for(variable in vec_variables) { if(!is.element(variable, colnames(data))) { stop(variable," is not in data") } }
 }
 
-# describe_data
+# Function use in describe_data.R
 split_data_for_ggplot = function(data, factor, nb_param){
   ns = unique(data[,factor])
   s = rep(c(1:length(ns)), each = nb_param)[1:length(ns)]
@@ -35,5 +35,25 @@ split_data_for_ggplot = function(data, factor, nb_param){
   return(data_f)
 }
 
-
+# Function use in which_won_where.R, mean_vs_stability.R
+get_biplot = function(res.pca){
+  
+  var = as.data.frame(res.pca$var$coord)
+  var = cbind.data.frame(rownames(var), var, color = "darkgreen"); colnames(var)[1:3] = c("label", "x", "y")
+  
+  ind = as.data.frame(res.pca$ind$coord)
+  ind = cbind.data.frame(rownames(ind), ind, color = "black"); colnames(ind)[1:3] = c("label", "x", "y")
+  
+  r <- min((max(ind[, "x"]) - min(ind[, "x"])/(max(var[, "x"]) - min(var[, "x"]))), (max(ind[, "y"]) - min(ind[, "y"])/(max(var[, "y"]) - min(var[, "y"]))))
+  var[, c("x", "y")] <- var[, c("x", "y")] * r * 0.7 # taken from factoextra::fviz_pca_biplot
+  
+  vi = rbind.data.frame(var, ind)
+  
+  dimvar = round(res.pca$eig$`percentage of variance`[1:2], 1)
+  
+  p = ggplot(data = vi, aes(x = x, y = y, label = label)) + geom_text(color = as.character(vi$color)) + geom_point(color = as.character(vi$color))
+  p = p + xlab(paste("Dim 1 (", dimvar[1], "%)", sep = "")) + ylab(paste("Dim 2 (", dimvar[2], "%)", sep = ""))
+  p = p + ggtitle("Biplot germplasm and environments")
+  return(p)
+}
 
