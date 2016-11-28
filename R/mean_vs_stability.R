53512e766b26177939fcdd23cb32eb9e4166180b
priviere <pierre@semencespaysannes.org>
2016-11-28 12:47:39 +0100

    correct the value of the score according to the arrow direction

--- R/mean_vs_stability.R
+++ R/mean_vs_stability.R
@@ -30,13 +30,12 @@ mean_vs_stability = function(res.pca){
   }
   colnames(per_line) = c("x1", "y1", "x2", "y2")
   
-  # !!!!!!!!!!!!!!!!!!!!!!!!!!!
-  per_line = arrange(per_line, x1) # !!! A améliorer et ok
-  # !!!!!!!!!!!!!!!!!!!!!!!!!!!
-  
+  per_line$score = sqrt(per_line$x1*per_line$x1 + per_line$y1*per_line$y1)
+  # the arrow point the greater value (i.e. greater score)
+  slope = xymean$y2 / xymean$x2
+  if( slope > 0 ){ per_line = arrange(per_line, -score) } else { per_line = arrange(per_line, score) }
   per_line$color_seg = c(1:nrow(per_line))
-  print(per_line)
-  
+
   p = p + geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2, color = color_seg), data = per_line, linetype = 2, size = 1, inherit.aes = FALSE)
   p = p + scale_colour_gradient(low = "green", high = "red")
   p = p + ggtitle("Mean vs stability")
