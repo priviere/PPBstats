233238c50b45df4fed817cda5b287f051432d532
priviere <pierre@semencespaysannes.org>
2016-12-02 14:28:32 +0100

    add coordinate x1 y1

--- R/ggplot_discrimitiveness_vs_representativeness.R
+++ R/ggplot_discrimitiveness_vs_representativeness.R
@@ -8,16 +8,19 @@ ggplot_discrimitiveness_vs_representativeness = function(res.pca){
   p = p + geom_segment(data = xymean, aes(x = x1, y = y1, xend = x2, yend = y2), arrow=arrow(length=unit(0.4,"cm")), color = "red", inherit.aes = FALSE)
   
   d = data.frame(x1 = xymean$x2, y1 = xymean$y2, x2 = var$x, y2 = var$y)
+  
   # distance of each location from the average location
   d$score = NA
   for(i in 1:nrow(d)){
+    x1 = d[i,"x1"]
+    y1 = d[i,"y1"]
     x2 = d[i,"x2"]
     y2 = d[i,"y2"]
     px = x2-x1
     py = y2-y1
     d[i,"score"] = px*px + py*py
   }
-  
+
   p = p + geom_segment(data = d, aes(x = x1, y = y1, xend = x2, yend = y2, color = score), linetype = 2, inherit.aes = FALSE)
   p = p + scale_colour_gradient(low = "green", high = "red")
   p = p + ggtitle("discrimitiveness vs representativeness")
