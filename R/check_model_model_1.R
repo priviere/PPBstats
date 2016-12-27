aee72c82cc5eefbfe30a6347d1887c77678b76a9
priviere <pierre@semencespaysannes.org>
2016-12-27 12:53:12 +0100

    Distribution of sigma_j in the inverse Gamme distribution

--- R/check_model_model_1.R
+++ R/check_model_model_1.R
@@ -55,51 +55,44 @@ check_model_model_1 = function(
     } else {   model1.data_env_whose_param_did_not_converge = NULL }
   }
   
+  # 3.Format MCMC for further use ----------
+  sq_MCMC$entry_mu = sub("mu\\[", "", sapply(sq_MCMC$parameter, function(x){unlist(strsplit(as.character(x), ","))[1]}))
+  
+  env_beta = sub("\\]", "", sub("beta\\[", "", sapply(sq_MCMC$parameter[grep("beta\\[", sq_MCMC$parameter)], function(x){unlist(strsplit(as.character(x), ","))[1]})))
+  env_mu = sub("\\]", "", sub("mu\\[", "", sapply(sq_MCMC$parameter[grep("mu\\[", sq_MCMC$parameter)], function(x){unlist(strsplit(as.character(x), ","))[2]})))
+  env_nu = env_rho = NA
+  env_sigma = sub("\\]", "", sub("sigma\\[", "", sq_MCMC$parameter[grep("sigma\\[", sq_MCMC$parameter)]))
+  
+  sq_MCMC$environment =  c(env_beta, env_mu, env_nu, env_rho, env_sigma)
+  sq_MCMC$location = sapply(sq_MCMC$environment, function(x){unlist(strsplit(as.character(x), ":"))[1]})
+  sq_MCMC$year = sapply(sq_MCMC$environment, function(x){unlist(strsplit(as.character(x), ":"))[2]})
   
   
-  # 4. posteriors ----------
+  # 4. posteriors data frame for ggplot ----------
+  
+  # 4.1. sigma_j distribution ----------   
+  if( length(grep("nu", rownames(sq_MCMC))) > 0 & length(grep("rho", rownames(sq_MCMC))) > 0 & length(grep("sigma", rownames(sq_MCMC))) > 0 ) {
+    nu = sq_MCMC["nu", "q3"]
+    rho = sq_MCMC["rho", "q3"]
+    d_sigma_distribution = cbind.data.frame(sigma_distribution = sqrt(1/rgamma(10000, nu, rho)))
+    sigma = sq_MCMC[grep("sigma", sq_MCMC$parameter), "q3"]
+    names(sigma) = sq_MCMC$parameter[grep("sigma", sq_MCMC$parameter)]
+    sigma = sort(sigma)
+    d_sigma = cbind.data.frame(sigma = as.factor(names(sigma)), value = sigma)
+    data_ggplot_model_1_sigma_j = list(d_sigma_distribution = d_sigma_distribution, d_sigma = d_sigma)
+  } else { data_ggplot_model_1_sigma_j = NULL }
+  
+  
+  
+  
+  
+  
+  
+  
   out.posteriors = NULL
   if(analysis == "all" | analysis == "posteriors") {
     
 
-    # 4.1.2. Format MCMC for further use ----------
-    sq_MCMC$entry_mu = sub("mu\\[", "", sapply(sq_MCMC$parameter, function(x){unlist(strsplit(as.character(x), ","))[1]}))
-    
-    env_beta = sub("\\]", "", sub("beta\\[", "", sapply(sq_MCMC$parameter[grep("beta\\[", sq_MCMC$parameter)], function(x){unlist(strsplit(as.character(x), ","))[1]})))
-    env_mu = sub("\\]", "", sub("mu\\[", "", sapply(sq_MCMC$parameter[grep("mu\\[", sq_MCMC$parameter)], function(x){unlist(strsplit(as.character(x), ","))[2]})))
-    env_nu = env_rho = NA
-    env_sigma = sub("\\]", "", sub("sigma\\[", "", sq_MCMC$parameter[grep("sigma\\[", sq_MCMC$parameter)]))
-    sq_MCMC$environment =  c(env_beta, env_mu, env_nu, env_rho, env_sigma)
-    
-    sq_MCMC$location = sapply(sq_MCMC$environment, function(x){unlist(strsplit(as.character(x), ":"))[1]})
-    sq_MCMC$year = sapply(sq_MCMC$environment, function(x){unlist(strsplit(as.character(x), ":"))[2]})
-    
-    # 4.1.3. sigma_j distribution ----------   
-    out_sigma_distribution = NULL
-    if( length(grep("nu", rownames(sq_MCMC))) > 0 & length(grep("rho", rownames(sq_MCMC))) > 0 & length(grep("sigma", rownames(sq_MCMC))) > 0 ) {
-      nu = sq_MCMC["nu", "q3"]
-      rho = sq_MCMC["rho", "q3"]
-      d_sigma_distribution = cbind.data.frame(sigma_distribution = sqrt(1/rgamma(10000, nu, rho)))
-      
-      p = ggplot()
-      p = p + geom_density(data = d_sigma_distribution, aes(x = sigma_distribution) )
-      
-      sigma = sq_MCMC[grep("sigma", sq_MCMC$parameter), "q3"]; names(sigma) = sq_MCMC$parameter[grep("sigma", sq_MCMC$parameter)]
-      sigma = sort(sigma)
-      d_sigma = cbind.data.frame(sigma = as.factor(names(sigma)), value = sigma)
-      
-      p.tmp = p + geom_vline(data = d_sigma, aes(xintercept = value, color = sigma))
-      out = list(p.tmp)
-      
-      SEQ = unique(c(seq(0, nrow(d_sigma), 5), nrow(d_sigma)))
-      for(s in 1:(length(SEQ) - 1)) {
-        d_sigma_tmp = d_sigma[c((SEQ[s]+1):SEQ[s+1]),]
-        p.tmp = p + geom_vline(data = d_sigma_tmp, aes(xintercept = value, color = sigma), show.legend = TRUE)
-        out = c(out, list(p.tmp))
-      }
-      out_sigma_distribution = out
-      message("The values of sigma in the inverse Gamme distribution are done.")
-    }
     
     # 4.1.4. mu_ij, beta_jk and sigma_j caterpillar plot distribution ----------
     out_para_posteriors = NULL
