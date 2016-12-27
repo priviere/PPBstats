ddca74d92f10c6144d75bbb58d0037a7ef90fb29
priviere <pierre@semencespaysannes.org>
2016-12-27 14:23:17 +0100

    update workflow for model 1

--- R/ggplot_check_model.R
+++ R/ggplot_check_model.R
@@ -1,5 +1,5 @@
 ggplot_check_model = function(
-  data_ggplot_check_model,
+  out_check_model,
   nb_parameters_per_plot = 10
   ){
   
@@ -12,54 +12,81 @@ ggplot_check_model = function(
     return(p)
   }
   
-  # model 1, sigma_j ----------
-  data_ggplot_model_1_sigma_j = list(d_sigma_distribution = d_sigma_distribution, d_sigma = d_sigma)  
-  p = ggplot()
-  p = p + geom_density(data = d_sigma_distribution, aes(x = sigma_distribution) )
-  p.tmp = p + geom_vline(data = d_sigma, aes(xintercept = value, color = sigma))
-  out = list(p.tmp)
+  # 1. model 1 ----------
+  if( attributes(out_check_model)$check_model == "model_1" ) {
+    data_ggplot = out_check_model$data_ggplot
+    data_ggplot_model_1_sigma_j = data_ggplot$sigma_j
+    data_ggplot_model_1_mu_ij = data_ggplot$mu_ij
+    data_ggplot_model_1_beta_jk = data_ggplot$beta_jk
+    data_ggplot_model_1_sigma_j_2 = data_ggplot$sigma_j_2
+    data_ggplot_model_1_epsilon_ijk = data_ggplot$epsilon_ijk
+    
+    # sigma_j ----------
+    if( !is.null(data_ggplot_model_1_sigma_j)){
+      d_sigma_distribution = data_ggplot_model_1_sigma_j$d_sigma_distribution
+      d_sigma = data_ggplot_model_1_sigma_j$d_sigma
+      
+      p = ggplot()
+      p = p + geom_density(data = d_sigma_distribution, aes(x = sigma_distribution) )
+      p.tmp = p + geom_vline(data = d_sigma, aes(xintercept = value, color = sigma))
+      out = list(p.tmp)
+      
+      SEQ = unique(c(seq(0, nrow(d_sigma), 5), nrow(d_sigma)))
+      for(s in 1:(length(SEQ) - 1)) {
+        d_sigma_tmp = d_sigma[c((SEQ[s]+1):SEQ[s+1]),]
+        p.tmp = p + geom_vline(data = d_sigma_tmp, aes(xintercept = value, color = sigma), show.legend = TRUE)
+        out = c(out, list(p.tmp))
+      }
+      out_sigma_j_gamma = out
+      message("Distribution of sigma_j in the inverse Gamme distribution are done.")
+    } else { out_sigma_j_gamma = NULL }
+    
+    # mu_ij caterpillar plot ----------
+    if(!is.null(data_ggplot_model_1_mu_ij)){
+      xmin = min(data_ggplot_model_1_mu_ij$q1); xmax = max(data_ggplot_model_1_mu_ij$q5)
+      data_ggplot_model_1_mu_ij = plyr:::splitter_d(sq_MCMC_mu, .(environment))
+      out = lapply(data_ggplot_model_1_mu_ij, function(x){ get.caterpillar.plot(x) }) # + xlim(xmin, xmax)
+      out_mu_ij = list("mu_posteriors" = out)
+      message("The mu_ij posterior distributions are done.")
+    } else { out_mu_ij = NULL }
+    
+    # beta_jk caterpillar plot ----------
+    if(!is.null(data_ggplot_model_1_beta_jk)){
+      xmin = min(data_ggplot_model_1_beta_jk$q1); xmax = max(data_ggplot_model_1_beta_jk$q5)
+      data_ggplot_model_1_beta_jk = plyr:::splitter_d(sq_MCMC_beta, .(environment))
+      out = lapply(data_ggplot_model_1_beta_jk, function(x){ get.caterpillar.plot(x) }) # + xlim(xmin, xmax)
+      out_beta_jk = list("beta_posteriors" = out)
+      message("The beta_jk posterior distributions are done.")      
+    } else { out_beta_jk = NULL }
+    
+    # sigma_j caterpillar plot ----------
+    if(!is.null(data_ggplot_model_1_sigma_j_2)){
+      xmin = min(data_ggplot_model_1_sigma_j_2$q1); xmax = max(data_ggplot_model_1_sigma_j_2$q5)
+      
+      data_ggplot_model_1_sigma_j_2$split = add_split_col(data_ggplot_model_1_sigma_j_2, nb_parameters_per_plot)
+      data_ggplot_model_1_sigma_j_2 = plyr:::splitter_d(sq_MCMC_sigma, .(split))
+      
+      out = lapply(data_ggplot_model_1_sigma_j, function(x){ get.caterpillar.plot(x) + ggtitle("") } ) # + xlim(xmin, xmax) 
+      out_sigma_j = list("sigma_posteriors" = out)
+      message("The sigma_j posterior distributions are done.")
+    } else { out_sigma_j = NULL }
+    
+    # standardized epsilon_ijk distribution ----------
+    if(!is.null(data_ggplot_model_1_epsilon_ijk)){
+      out_epsilon_ijk = ggplot(data_ggplot_model_1_epsilon_ijk, aes(x = x, y = std_res)) + geom_point() + xlab("") + ylab("standardised residuals")
+      message("The standardised residuals distributions are done.")
+    } else { out_epsilon_ijk = NULL }
   
-  SEQ = unique(c(seq(0, nrow(d_sigma), 5), nrow(d_sigma)))
-  for(s in 1:(length(SEQ) - 1)) {
-    d_sigma_tmp = d_sigma[c((SEQ[s]+1):SEQ[s+1]),]
-    p.tmp = p + geom_vline(data = d_sigma_tmp, aes(xintercept = value, color = sigma), show.legend = TRUE)
-    out = c(out, list(p.tmp))
+    out_model_1 = list(
+      out_sigma_j_gamma,
+      out_mu_ij,
+      out_beta_jk,
+      out_sigma_j,
+      out_epsilon_ijk
+    )
+      
   }
-  out_sigma_distribution = out
-  message("Distribution of sigma_j in the inverse Gamme distribution are done.")
   
-  # model 1, mu_ij caterpillar plot
-  xmin = min(data_ggplot_model_1_mu_ij$q1); xmax = max(data_ggplot_model_1_mu_ij$q5)
-  data_ggplot_model_1_mu_ij = plyr:::splitter_d(sq_MCMC_mu, .(environment))
-  out = lapply(data_ggplot_model_1_mu_ij, function(x){ get.caterpillar.plot(x) }) # + xlim(xmin, xmax)
-  out = list("mu_posteriors" = out)
-  out_para_posteriors = c(out_para_posteriors, out)
-  message("The mu_ij posterior distributions are done.")
-  
-  # model 1, beta_jk caterpillar plot ----------
-  xmin = min(data_ggplot_model_1_beta_jk$q1); xmax = max(data_ggplot_model_1_beta_jk$q5)
-  data_ggplot_model_1_beta_jk = plyr:::splitter_d(sq_MCMC_beta, .(environment))
-  out = lapply(data_ggplot_model_1_beta_jk, function(x){ get.caterpillar.plot(x) }) # + xlim(xmin, xmax)
-  out = list("beta_posteriors" = out)
-  out_para_posteriors = c(out_para_posteriors, out)
-  message("The beta_jk posterior distributions are done.")      
-  
-  
-  # model 1, sigma_j caterpillar plot ----------
-  xmin = min(data_ggplot_model_1_sigma_j_2$q1); xmax = max(data_ggplot_model_1_sigma_j_2$q5)
-  
-  data_ggplot_model_1_sigma_j_2$split = add_split_col(data_ggplot_model_1_sigma_j_2, nb_parameters_per_plot)
-  data_ggplot_model_1_sigma_j_2 = plyr:::splitter_d(sq_MCMC_sigma, .(split))
-  
-  out = lapply(data_ggplot_model_1_sigma_j, function(x){ get.caterpillar.plot(x) + ggtitle("") } ) # + xlim(xmin, xmax) 
-  out = list("sigma_posteriors" = out)
-  out_para_posteriors = c(out_para_posteriors, out)
-  message("The sigma_j posterior distributions are done.")
-  
-  
-  # model 1, standardized epsilon_ijk distribution ----------
-  out_stand_res = ggplot(data_ggplot_model_1_epsilon_ijk, aes(x = x, y = std_res)) + geom_point() + xlab("") + ylab("standardised residuals")
-  message("The standardised residuals distributions are done.")
   
   
 }
