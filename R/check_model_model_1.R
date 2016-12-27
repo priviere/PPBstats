958c4c51cb82788b19ddddb5357abdc47c6b4365
priviere <pierre@semencespaysannes.org>
2016-12-27 14:28:40 +0100

    update sectionning

--- R/check_model_model_1.R
+++ R/check_model_model_1.R
@@ -3,7 +3,7 @@ check_model_model_1 = function(
 )
   {
   
-  # 2. Convergence, update MCMC and data frame with environments where some parameters did not converge ----------
+  # 1. Convergence, update MCMC and data frame with environments where some parameters did not converge ----------
   out.conv = check_convergence(out.model, model_name = "model1")
   MCMC = out.conv$MCMC
   sq_MCMC = out.conv$sq_MCMC
@@ -51,7 +51,7 @@ check_model_model_1 = function(
     } else {   model1.data_env_whose_param_did_not_converge = NULL }
   } else {   model1.data_env_whose_param_did_not_converge = NULL }
   
-  # 3. posteriors data frame for ggplot ----------
+  # 2. posteriors data frame for ggplot ----------
   
   sq_MCMC$entry_mu = sub("mu\\[", "", sapply(sq_MCMC$parameter, function(x){unlist(strsplit(as.character(x), ","))[1]}))
   
@@ -64,7 +64,7 @@ check_model_model_1 = function(
   sq_MCMC$location = sapply(sq_MCMC$environment, function(x){unlist(strsplit(as.character(x), ":"))[1]})
   sq_MCMC$year = sapply(sq_MCMC$environment, function(x){unlist(strsplit(as.character(x), ":"))[2]})
   
-  # 3.1. sigma_j distribution ----------   
+  # 2.1. sigma_j distribution ----------   
   if( length(grep("nu", rownames(sq_MCMC))) > 0 & length(grep("rho", rownames(sq_MCMC))) > 0 & length(grep("sigma", rownames(sq_MCMC))) > 0 ) {
     nu = sq_MCMC["nu", "q3"]
     rho = sq_MCMC["rho", "q3"]
@@ -76,22 +76,22 @@ check_model_model_1 = function(
     data_ggplot_model_1_sigma_j = list(d_sigma_distribution = d_sigma_distribution, d_sigma = d_sigma)
   } else { data_ggplot_model_1_sigma_j = NULL }
   
-  # 3.2. mu_ij caterpillar plot ----------
+  # 2.2. mu_ij caterpillar plot ----------
   if ( length(grep("mu", rownames(sq_MCMC))) > 0  ) {
     data_ggplot_model_1_mu_ij = droplevels(sq_MCMC[grep("mu", rownames(sq_MCMC)),])
   } else { data_ggplot_model_1_mu_ij = NULL }
   
-  # 3.3. beta_jk caterpillar plot ----------
+  # 2.3. beta_jk caterpillar plot ----------
   if ( length(grep("beta", rownames(sq_MCMC))) > 0  ) {
     data_ggplot_model_1_beta_jk = droplevels(sq_MCMC[grep("beta", rownames(sq_MCMC)),])   
   } else { data_ggplot_model_1_beta_jk = NULL }
   
-  # 3.4. sigma_j caterpillar plot ----------
+  # 2.4. sigma_j caterpillar plot ----------
   if ( length(grep("sigma", rownames(sq_MCMC))) > 0  ) {
     data_ggplot_model_1_sigma_j_2 = droplevels(sq_MCMC[grep("sigma", rownames(sq_MCMC)),])
   } else { data_ggplot_model_1_sigma_j_2 = NULL }
   
-  # 3.5. standardized epsilon_ijk distribution ----------
+  # 2.5. standardized epsilon_ijk distribution ----------
   if ( !is.null(out.model$epsilon)  ) {
     epsilon_ijk = out.model$epsilon
     sigma_j = sq_MCMC[grep("sigma", sq_MCMC$parameter), "q3"]
@@ -102,7 +102,7 @@ check_model_model_1 = function(
   } else { data_ggplot_model_1_epsilon_ijk = NULL }
   
 
-  # 4. Return outptus ----------
+  # 3. Return results ----------
   out = list(
     "MCMC" = MCMC,
     "data_env_whose_param_did_not_converge" = model1.data_env_whose_param_did_not_converge,
