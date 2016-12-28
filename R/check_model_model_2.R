ce161d2ef7e39c4bd58393b911690f512be539fa
priviere <pierre@semencespaysannes.org>
2016-12-28 13:17:27 +0100

    update regarding model 2 code

--- R/check_model_model_2.R
+++ R/check_model_model_2.R
@@ -2,14 +2,11 @@ check_model_model_2 = function(
   out.model,
   nb_parameters_per_plot = 10
 ){
-  # 1. Error message and update arguments ----------
-  analysis = check_analysis_argument(analysis)
-  
   # Default settings
   model2.presence.abscence.matrix = out.model$model2.presence.abscence.matrix
   
-  # 2. Convergence, update MCMC when parameters did not converge ----------
-  out.convergence = check_convergence(out.model, model_name = "model2")
+  # 1. Convergence, update MCMC when parameters did not converge ----------
+  out.conv = check_convergence(out.model, model_name = "model2")
   MCMC = out.conv$MCMC
   sq_MCMC = out.conv$sq_MCMC
   out.convergence = out.conv$out.convergence
@@ -47,83 +44,46 @@ check_model_model_2 = function(
     attributes(MCMC)$model = "model2"
   }
   
-  # 4. posteriors ----------
+  # 2. posteriors data frame for ggplot ----------
   
-  out.posteriors = NULL
-  if(analysis == "all" | analysis == "posteriors") {
-    
-    if(attributes(out.model)$PPBstats.object == "model2") {
-      
-      # 4.2.1. Update MCMC and model2.presence.abscence.matrix ----------
-      if(analysis == "all" | analysis == "convergence") {
-
-      }
-      
-      # 4.2.2. alpha_i, beta_i, theta_j caterpillar plot distribution ----------
-      out_para_posteriors = NULL
-      
-      if ( length(grep("alpha\\[", rownames(sq_MCMC))) > 0  ) {      
-        sq_MCMC_alpha = droplevels(sq_MCMC[grep("alpha\\[", rownames(sq_MCMC)),]) 
-        xmin = min(sq_MCMC_alpha$q1); xmax = max(sq_MCMC_alpha$q5)
-        
-        sq_MCMC_alpha$split = add_split_col(sq_MCMC_alpha, each = nb_parameters_per_plot)
-        sq_MCMC_alpha_split = plyr:::splitter_d(sq_MCMC_alpha, .(split))      
-        
-        out = lapply(sq_MCMC_alpha_split, function(x){ get.caterpillar.plot(x) } ) # + xlim(xmin, xmax) 
-        out = list("alpha_posteriors" = out)
-        out_para_posteriors = c(out_para_posteriors, out)
-        message("The alpha_i posterior distributions are done.")
-      }
-      
-      if ( length(grep("beta\\[", rownames(sq_MCMC))) > 0  ) {
-        sq_MCMC_beta = droplevels(sq_MCMC[grep("beta\\[", rownames(sq_MCMC)),])    
-        xmin = min(sq_MCMC_beta$q1); xmax = max(sq_MCMC_beta$q5)
-        
-        sq_MCMC_beta$split = add_split_col(sq_MCMC_beta, each = nb_parameters_per_plot)
-        sq_MCMC_beta_split = plyr:::splitter_d(sq_MCMC_beta, .(split))      
-        
-        out = lapply(sq_MCMC_beta_split, function(x){ get.caterpillar.plot(x) } ) # + xlim(xmin, xmax) 
-        out = list("beta_posteriors" = out)
-        out_para_posteriors = c(out_para_posteriors, out)
-        message("The beta_i posterior distributions are done.")      
-      }
-      
-      if ( length(grep("theta\\[", rownames(sq_MCMC))) > 0  ) {
-        sq_MCMC_theta = droplevels(sq_MCMC[grep("theta\\[", rownames(sq_MCMC)),])    
-        xmin = min(sq_MCMC_theta$q1); xmax = max(sq_MCMC_theta$q5)
-        
-        sq_MCMC_theta$split = add_split_col(sq_MCMC_theta, each = nb_parameters_per_plot)
-        sq_MCMC_theta_split = plyr:::splitter_d(sq_MCMC_theta, .(split))      
-        
-        out = lapply(sq_MCMC_theta_split, function(x){ get.caterpillar.plot(x) } ) # + xlim(xmin, xmax)
-        out = list("theta_posteriors" = out)
-        out_para_posteriors = c(out_para_posteriors, out)
-        message("The theta_j posterior distributions are done.")
-      }
-      
-      # 4.2.3. standardized epsilon_ijk distribution ----------
-      out_stand_res = NULL
-      
-      if ( !is.null(out.model$epsilon)  ) {      
-        
-        epsilon_ijk = out.model$epsilon     
-        sigma_epsilon = sq_MCMC[grep("sigma_epsilon", sq_MCMC$parameter), "q3"]
-        std_res = epsilon_ijk / sigma_epsilon
-        
-        d_std_res = cbind.data.frame(x = c(1:length(epsilon_ijk)), std_res)
-        out_stand_res = ggplot(d_std_res, aes(x = x, y = std_res)) + geom_point() + xlab("") + ylab("standardised residuals")
-        message("The standardised residuals distributions are done.")
-      }
-      
-      out.posteriors = list("parameter_posteriors" = out_para_posteriors, "standardized_residuals" = out_stand_res)    
-    }
-    
+  # 2.1. alpha_i caterpillar plot distribution ----------
+  if ( length(grep("alpha\\[", rownames(sq_MCMC))) > 0  ) {      
+    data_ggplot_model_2_alpha = droplevels(sq_MCMC[grep("alpha\\[", rownames(sq_MCMC)),]) 
   }
-  # 5. Return outptus ----------
-  out = list("model2.presence.abscence.matrix" = model2.presence.abscence.matrix,
-             "convergence" = out.convergence, 
-             "posteriors" = out.posteriors, 
-             "MCMC" = MCMC)
-  return(out)
   
+  # 2.2. beta_i caterpillar plot distribution ----------
+  if ( length(grep("beta\\[", rownames(sq_MCMC))) > 0  ) {
+    data_ggplot_model_2_beta = droplevels(sq_MCMC[grep("beta\\[", rownames(sq_MCMC)),])    
+  }
+  
+  # 2.3. theta_j caterpillar plot distribution ----------
+  if ( length(grep("theta\\[", rownames(sq_MCMC))) > 0  ) {
+    data_ggplot_model_2_theta = droplevels(sq_MCMC[grep("theta\\[", rownames(sq_MCMC)),])    
+  }
+  
+  # 2.4. standardized epsilon_ijk distribution ----------
+  if ( !is.null(out.model$epsilon)  ) {      
+    epsilon_ijk = out.model$epsilon     
+    sigma_epsilon = sq_MCMC[grep("sigma_epsilon", sq_MCMC$parameter), "q3"]
+    std_res = epsilon_ijk / sigma_epsilon
+    data_ggplot_model_2_epsilon = cbind.data.frame(x = c(1:length(epsilon_ijk)), std_res)
+  }
+  
+
+  
+  # 3. Return results ----------
+  out = list(
+    "MCMC" = MCMC,
+    "model2.presence.abscence.matrix" = model2.presence.abscence.matrix,
+    "data_ggplot" = list(
+      "alpha" = data_ggplot_model_2_alpha,
+      "beta" = data_ggplot_model_2_beta,
+      "theta" = data_ggplot_model_2_theta,
+      "epsilon" = data_ggplot_model_2_epsilon
+    )
+  )
+
+  attributes(out)$PPBstats.object = "check_model_model_2"
+  
+  return(out)
 }
