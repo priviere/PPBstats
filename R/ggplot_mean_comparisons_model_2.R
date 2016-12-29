13024fed01101adf71dec3f6717a28b5f9890720
priviere <pierre@semencespaysannes.org>
2016-12-29 19:02:06 +0100

    add new fonction

---
+++ R/ggplot_mean_comparisons_model_2.R
@@ -0,0 +1,17 @@
+ggplot_mean_comparisons_model_2 = function(
+  mean_comparisons_model_2,
+  nb_parameters_per_plot = 10
+){
+  
+  # 1. Error message
+  if( attributes(mean_comparisons_model_1)$PPBstats.object != "mean_comparisons_model_2" ) { stop("data must come from mean_comparisons and model_2") }
+  
+  
+  # return results
+  out = list(
+    
+  )
+  
+  return(out)
+}
+
