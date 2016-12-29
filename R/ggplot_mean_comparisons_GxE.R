ee75bb6b551f2bd9e7ed9b95a33f5ff5af33258a
priviere <pierre@semencespaysannes.org>
2016-12-29 13:50:16 +0100

    add fac arg

--- R/ggplot_mean_comparisons_GxE.R
+++ R/ggplot_mean_comparisons_GxE.R
@@ -6,11 +6,13 @@ ggplot_mean_comparisons_GxE = function(
   # 1. Error message
   if( attributes(data)$PPBstats.object != "mean_comparisons_GxE" ) { stop("data must come from mean_comparisons and GxE") }
   
+  variable = data$variable
+  
   data_ggplot_LSDbarplot_germplasm = data$data_ggplot_LSDbarplot_germplasm
   data_ggplot_LSDbarplot_location = data$data_ggplot_LSDbarplot_location
   data_ggplot_LSDbarplot_year = data$data_ggplot_LSDbarplot_year
   
-  ggplot_LSDbarplot = function(d_LSD, variable){
+  ggplot_LSDbarplot = function(d_LSD, fac, variable){
     p = ggplot(data = d_LSD, aes(x = trt, y = means, label = M)) + geom_bar(stat = "identity")
     p = p + geom_text(aes(y = ymean), angle = 90, color = "white")
     p = p + theme(legend.position = "none", axis.text.x = element_text(angle = 90))
@@ -19,13 +21,13 @@ ggplot_mean_comparisons_GxE = function(
   }
   
   # 2. Germplasm
-  ggplot_LSDbarplot_germplasm = ggplot_LSDbarplot(data_ggplot_LSDbarplot_germplasm, variable)
+  ggplot_LSDbarplot_germplasm = ggplot_LSDbarplot(data_ggplot_LSDbarplot_germplasm, "germplasm", variable)
   
   # 3. Location
-  ggplot_LSDbarplot_location = ggplot_LSDbarplot(data_ggplot_LSDbarplot_location, variable)
+  ggplot_LSDbarplot_location = ggplot_LSDbarplot(data_ggplot_LSDbarplot_location, "location", variable)
   
   # 4. Year
-  ggplot_LSDbarplot_year = ggplot_LSDbarplot(data_ggplot_LSDbarplot_year, variable)
+  ggplot_LSDbarplot_year = ggplot_LSDbarplot(data_ggplot_LSDbarplot_year, "year", variable)
   
   # 5. return results
   out = list(
