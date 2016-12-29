2d5abe4a79741cb1a0f3a1671eb834f04e1615c8
priviere <pierre@semencespaysannes.org>
2016-12-29 13:13:29 +0100

    firs try

--- R/GxE_biplot.R
+++ R/GxE_biplot.R
@@ -1,5 +1,25 @@
-GxE_biplot = function(){
+GxE_biplot = function(data){
   
+  # 1. Error message
+  if( attributes(data)$PPBstats.object != "check_model_GxE" ) { stop("data must come from check_model and GxE") }
   
+  # 2. Ecovalence ----------
+  m_eco = data_interaction^2
+  
+  data_ecovalence = data.frame(
+    germplasm = rep(rownames(m_eco), times = ncol(m_eco)), 
+    location = rep(colnames(m_eco), each = nrow(m_eco)),
+    variable = as.vector(m_eco)
+  )
+  
+  out = list(
+    "data_ecovalence" = data_ecovalence,
+    "pca" = data$PCA
+  )
+  
+  attributes(out)$PPBstats.object = "biplot_GxE"
   
 }
+
+
+
