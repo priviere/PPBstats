827e2e1c3b39beefa65e83221680f1c2bda07cd9
priviere <pierre@semencespaysannes.org>
2016-12-29 16:32:38 +0100

    first try + in comments code to handle

--- R/ggplot_parameter_groups.R
+++ R/ggplot_parameter_groups.R
@@ -1,4 +1,33 @@
-ggplot_parameter_groups = function(){
+ggplot_parameter_groups = function(parameter_groups_GxE){
+
+  PCA_G = parameter_groups_GxE$PCA_G
+  PCA_intraG = parameter_groups_GxE$PCA_intraG
+  PCA_E = parameter_groups_GxE$PCA_E
   
+  PCA_G = list(
+    "variation_dim" = fviz_eig(PCA_G),
+    "ind" = fviz_pca_ind(PCA_G),
+    "var" = fviz_pca_var(PCA_G)
+  )
+  
+  PCA_intraG = list(
+    "variation_dim" = fviz_eig(PCA_intraG),
+    "ind" = fviz_pca_ind(PCA_intraG),
+    "var" = fviz_pca_var(PCA_intraG)
+  )
+  
+  PCA_E = list(
+    "variation_dim" = fviz_eig(PCA_E),
+    "ind" = fviz_pca_ind(PCA_E),
+    "var" = fviz_pca_var(PCA_E)
+  )
+  
+  out = list(
+    "PCA_G_effect" = PCA_G,
+    "PCA_intraG_effect" = PCA_intraG,
+    "PCA_E_effect" = PCA_E
+  )
+  
+  return(out)
   
 }
