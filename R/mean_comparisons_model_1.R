3c608955b1c38a0a6d5aaeaf3780b5a15e61fb24
priviere <pierre@semencespaysannes.org>
2016-12-29 21:34:41 +0100

    update code regarding mean_comparisons

--- R/mean_comparisons_model_1.R
+++ R/mean_comparisons_model_1.R
@@ -1,16 +1,33 @@
 mean_comparisons_model_1 = function(
-  out_check_model, 
-  p.adj = "none"
+  out_check_model_1,
+  parameter,
+  alpha = 0.05,
+  type = 1,
+  threshold = 1,
+  p.adj = "soft.bonf",
+  get.at.least.X.groups = 2,
+  precision = 0.0005
 ){
   # 1. Error message
-  if( attributes(out_check_model)$PPBstats.object != "check_model_model_2" ) { stop("data must come from check_model and model_2") }
+  if( attributes(out_check_model_1)$PPBstats.object != "check_model_model_1" ) { stop("data must come from check_model and model_1") }
   
+  if(!is.element(parameter, c("mu", "beta"))) { stop("With outputs from model 1, the parameters must be mu or beta.") }
+  
+  # 2. Get square matrice with pvalue or vector with pvalue ----------
+  MCMC_par = function(MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups){
+    a = colnames(MCMC)[grep(parameter, colnames(MCMC))]
+    vec_env = sub("\\]", "", unique(sapply(a, function(x){unlist(strsplit(x, ","))[2]})))
+    vec_MCMC_par = lapply(vec_env, function(env, MCMC){ MCMC[grep(paste(",", env, "]", sep = ""), colnames(MCMC))] }, MCMC)
+    out = lapply(vec_MCMC_par, get_mean_comparisons_and_Mpvalue, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups) 
+    names(out) = vec_env
+    return(out)
+  }
+  
+  if(parameter == "mu") { out = MCMC_par(out_check_model_1$MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups) }
+  
+  if(parameter == "beta") { out = MCMC_par(out_check_model_1$MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups) }
   
   # return results
-  out = list(
-    
-  )
-  
   attributes(out)$PPBstats.object = "mean_comparisons_model_1"
   
   return(out)
--- R/mean_comparisons_model_2.R
+++ R/mean_comparisons_model_2.R
@@ -1,17 +1,32 @@
 mean_comparisons_model_2 = function(
-  out_check_model, 
-  p.adj = "none"
+  out_check_model_2, 
+  parameter,
+  alpha = 0.05,
+  type = 1,
+  threshold = 1,
+  p.adj = "soft.bonf",
+  get.at.least.X.groups = 2,
+  precision = 0.0005
   ){
   # 1. Error message
-  if( attributes(out_check_model)$PPBstats.object != "check_model_model_2" ) { stop("data must come from check_model and model_2") }
+  if(!is.element(parameter, c("alpha", "beta", "theta"))) { stop("With outputs from model 2, the parameters must be alpha, beta or theta.") }
+  
+  # 2. Get square matrice with pvalue or vector with pvalue ----------
+  MCMC_par = function(MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups){
+    MCMC_par = MCMC[,grep(parameter, colnames(MCMC))]
+    out = get_mean_comparisons_and_Mpvalue(MCMC_par, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups) 
+    return(out)
+  }
+  
+  if(parameter == "alpha") { out = MCMC_par(out_check_model_2$MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups) }
+  
+  if(parameter == "beta") { out = MCMC_par(out_check_model_2$MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups) }
+
+  if(parameter == "theta") { out = MCMC_par(out_check_model_2$MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups) }
   
 
   # return results
-  out = list(
-    
-  )
-  
   attributes(out)$PPBstats.object = "mean_comparisons_model_2"
-  
+
  return(out)
 }
