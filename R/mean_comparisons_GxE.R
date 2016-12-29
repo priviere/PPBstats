6ab8e41052b51329bee1d4a6ca7db87d635192c7
priviere <pierre@semencespaysannes.org>
2016-12-29 08:24:16 +0100

    add new function

---
+++ R/mean_comparisons_GxE.R
@@ -0,0 +1,11 @@
+mean_comparisons_GxE = function(){
+  "barplot_LSD_significant_group" = ggplot_LSDbarplot(model, variable, "germplasm", "bonferroni")
+  "barplot_LSD_significant_group" = ggplot_LSDbarplot(model, variable, "location", "bonferroni")
+  "barplot_LSD_significant_group" = ggplot_LSDbarplot(model, variable, "year", "bonferroni")
+  
+  
+  
+  
+  
+  
+}
