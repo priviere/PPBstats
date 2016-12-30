c1598e3a38cac278b1cb40011e5fd971c2024462
priviere <pierre@semencespaysannes.org>
2016-12-30 07:21:34 +0100

    transfert score for model1

--- R/get.ggplot.R
+++ R/get.ggplot.R
@@ -530,40 +530,6 @@ get.ggplot = function(
   }
   
   # 2.3. score ----------
-  if(ggplot.type == "score") {  
-    
-    d_loc_out = get.loc.year(data, nb_parameters_per_plot)
-    OUT = lapply(d_loc_out, function(x){
-      out = lapply(x, function(env){
-        # assign a number according to the group
-        vec_letters = sort(unique(unlist(sapply(as.character(env[,"groups"]), function(x){unlist(strsplit(x, ""))}))), decreasing = TRUE)
-        
-        SCORE = c(1:1000)
-        SEQ = sort(unique(c(seq(1, length(SCORE), floor(length(SCORE)/length(vec_letters))), max(SCORE))), decreasing = TRUE)
-        GP = as.character(env[,"groups"])
-        gp_nb = rep(0, length(GP))
-        
-        for(l in 1:length(vec_letters)) {
-          b = grep(vec_letters[l], GP)
-          GP[b] = "" 
-          gp_nb[b] = SEQ[l]
-        }
-        env$score = gp_nb  
-        
-        alpha.info = paste(env$alpha, "|", env$alpha.correction)
-        env$alpha.info_year = paste(env$year, alpha.info, sep = " - ")
-        
-        p = ggplot(env, aes(y = entry, x = alpha.info_year)) + geom_tile(aes(fill = score))
-        p = p + scale_fill_gradient(low = "blue",high = "red")
-        p = p + xlab("") + ylab("") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle(env[1, "location"])
-        return(p)
-      })
-      return(out)
-    })
-    
-    names(OUT) = names(d_loc_out)
-  }
-  
   
   # 2.4. PCA ----------
   if(ggplot.type == "PCA"){  
