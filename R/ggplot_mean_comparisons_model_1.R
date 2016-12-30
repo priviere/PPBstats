0b3ab961c0bf25e6dbc33730717ac11698baabe5
priviere <pierre@semencespaysannes.org>
2016-12-30 15:35:24 +0100

    debug and update score

--- R/ggplot_mean_comparisons_model_1.R
+++ R/ggplot_mean_comparisons_model_1.R
@@ -214,7 +214,10 @@ ggplot_mean_comparisons_model_1 = function(
   
   if(ggplot.type == "score") {  
     
-    data = data_mean_comparisons
+    data = all_data$data_mean_comparisons
+    dtmp = data.frame()
+    for(i in 1:length(data)){ dtmp = rbind.data.frame(dtmp, data[[i]]$mean.comparisons) }
+    data = dtmp
     
     get.loc.year = function(data, nb_parameters_per_plot){
       
@@ -232,41 +235,44 @@ ggplot_mean_comparisons_model_1 = function(
       return(d_loc_b)
     }
     
+    
     d_loc_out = get.loc.year(data, nb_parameters_per_plot)
     
-    OUT = lapply(d_loc_out, function(x){
-      out = lapply(x, function(env){
-        # assign a number according to the group
-        vec_letters = sort(unique(unlist(sapply(as.character(env[,"groups"]), function(x){unlist(strsplit(x, ""))}))), decreasing = TRUE)
-        
-        SCORE = c(1:1000)
-        SEQ = sort(unique(c(seq(1, length(SCORE), floor(length(SCORE)/length(vec_letters))), max(SCORE))), decreasing = TRUE)
-        GP = as.character(env[,"groups"])
-        gp_nb = rep(0, length(GP))
-        
-        for(l in 1:length(vec_letters)) {
-          b = grep(vec_letters[l], GP)
-          GP[b] = "" 
-          gp_nb[b] = SEQ[l]
-        }
-        env$score = gp_nb  
-        
-        alpha.info = paste(env$alpha, "|", env$alpha.correction)
-        env$alpha.info_year = paste(env$year, alpha.info, sep = " - ")
-        
-        p = ggplot(env, aes(y = entry, x = alpha.info_year)) + geom_tile(aes(fill = score))
-        p = p + scale_fill_gradient(low = "blue",high = "red")
-        p = p + xlab("") + ylab("") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle(env[1, "location"])
-        return(p)
-      })
-      return(out)
-    })
     
-    names(OUT) = names(d_loc_out)
+    out = lapply(d_loc_out, function(x){
+        lapply(x, function(env){
+          # assign a number according to the group
+          vec_letters = sort(unique(unlist(sapply(as.character(env[,"groups"]), function(x){unlist(strsplit(x, ""))}))), decreasing = TRUE)
+          
+          SCORE = c(1:1000)
+          SEQ = sort(unique(c(seq(1, length(SCORE), floor(length(SCORE)/length(vec_letters))), max(SCORE))), decreasing = TRUE)
+          GP = as.character(env[,"groups"])
+          gp_nb = rep(0, length(GP))
+          
+          for(l in 1:length(vec_letters)) {
+            b = grep(vec_letters[l], GP)
+            GP[b] = "" 
+            gp_nb[b] = SEQ[l]
+          }
+          env$score = gp_nb  
+          
+          alpha.info = paste(env$alpha, "|", env$alpha.correction)
+          env$alpha.info_year = paste(env$year, alpha.info, sep = " - ")
+          
+          p = ggplot(env, aes(y = entry, x = alpha.info_year)) + geom_tile(aes(fill = score))
+          p = p + scale_fill_gradient(low = "blue",high = "red")
+          p = p + xlab("") + ylab("") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle(env[1, "location"])
+          return(p)
+        })
+      })
+    
+    names(out) = names(d_loc_out)
   }
-  
   
   # return results
   return(out)
 }
+
+
+
 
