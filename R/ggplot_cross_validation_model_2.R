744fde021d0a60aa70434b958d668e522de6f70a
priviere <pierre@semencespaysannes.org>
2016-12-31 13:06:45 +0100

    add news functions

---
+++ R/ggplot_cross_validation_model_2.R
@@ -0,0 +1,7 @@
+ggplot_cross_validation_model_2 = function(
+  out_check_model_2,
+  nb_parameters_per_plot = 10
+){
+  
+  return(out)
+}
---
+++ R/ggplot_predict_the_past_model_2.R
@@ -0,0 +1,7 @@
+ggplot_predict_the_past_model_2 = function(
+  out_check_model_2,
+  nb_parameters_per_plot = 10
+){
+  
+  return(out)
+}
