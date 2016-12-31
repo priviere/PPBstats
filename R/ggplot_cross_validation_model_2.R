e432cb204038df8f413569093fb279c7523bd800
priviere <pierre@semencespaysannes.org>
2016-12-31 13:18:27 +0100

    paste plot

--- R/ggplot_cross_validation_model_2.R
+++ R/ggplot_cross_validation_model_2.R
@@ -1,7 +1,17 @@
 ggplot_cross_validation_model_2 = function(
-  out_check_model_2,
-  nb_parameters_per_plot = 10
+  out_cross_validation_model_2
 ){
+  
+  # 5. Get the regression ----------
+  p = ggplot(d_out, aes(x = real.value, y = estimated.value)) 
+  p = p + stat_smooth(method = "lm") + geom_point() + xlab("observed value")
+  model = lm(real.value ~ estimated.value)
+  out_r = list("plot" = p, "anova" = model)
+  
+  # 6. Get the confidence in the estimation ----------
+  bias = real.value - estimated.value
+  test = t.test(bias, mu = 0)
+  percentage.of.confidence = round(test$p.value * 100, 1) # probability than the mean is equal to zero  
   
   return(out)
 }
