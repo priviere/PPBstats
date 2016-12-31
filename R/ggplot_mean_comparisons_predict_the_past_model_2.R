f19aec15cf71a99930e1daffa2205384b8abfdc9
priviere <pierre@semencespaysannes.org>
2016-12-31 18:05:59 +0100

    debug

--- R/ggplot_mean_comparisons_predict_the_past_model_2.R
+++ R/ggplot_mean_comparisons_predict_the_past_model_2.R
@@ -6,7 +6,7 @@ ggplot_mean_comparisons_predict_the_past_model_2 = function(
 ){
   if( attributes(out_mean_comparisons_predict_the_past_model_2)$PPBstats.object != "mean_comparisons_predict_the_past_model_2" ) { stop("data must come from mean_comparisons_predict_the_past_model_2") }
   
-  attributes(out_mean_comparisons_predict_the_past_model_2)$PPBstats.object == "mean_comparisons_model_1"
+  attributes(out_mean_comparisons_predict_the_past_model_2)$PPBstats.object = "mean_comparisons_model_1"
   
   out = ggplot_mean_comparisons_model_1(out_mean_comparisons_predict_the_past_model_2, data_version, ggplot.type, nb_parameters_per_plot)
   
