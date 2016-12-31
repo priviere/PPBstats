139eb68c744e6515dccde93c50d24982a4154923
priviere <pierre@semencespaysannes.org>
2016-12-31 09:34:22 +0100

    update arg and input

--- R/ggplot_parameter_groups.R
+++ R/ggplot_parameter_groups.R
@@ -1,8 +1,9 @@
-ggplot_parameter_groups = function(parameter_groups){
+ggplot_out_parameter_groups = function(out_parameter_groups){
 
-  pca = parameter_groups$obj.pca
-  clust = parameter_groups$clust
+  pca = out_parameter_groups$obj.pca
   
+  clust = out_parameter_groups$clust
+  out = clust$out
   nb.k = clust$nb.k
   res.hc = clust$res.hc
   km.res = clust$km.res
@@ -15,7 +16,7 @@ ggplot_parameter_groups = function(parameter_groups){
       "var" = fviz_pca_var(pca)
     ),
     "clust" = list(
-      "nb_k" = fviz_nbclust(out, kmeans, method = "silhouette"),0
+      "nb_k" = fviz_nbclust(out, kmeans, method = "silhouette"),
       "dendrogramm" = fviz_dend(res.hc, cex = 0.5, k = nb.k, type = "triangle"),
       "pca" = fviz_cluster(km.res, out, frame.type = "norm")
     )
