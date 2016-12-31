baa9458c583c27d57958b783e6f126ef3892eb24
priviere <pierre@semencespaysannes.org>
2016-12-31 15:21:04 +0100

    update code

--- R/ggplot_mean_comparisons_predict_the_past_model_2.R
+++ R/ggplot_mean_comparisons_predict_the_past_model_2.R
@@ -1,2 +1,15 @@
-ggplot_mean_comparisons_predict_the_past_model_2 = ggplot_mean_comparisons_model_1
-
+ggplot_mean_comparisons_predict_the_past_model_2 = function(
+  out_mean_comparisons_predict_the_past_model_2,
+  data_version = NULL,
+  ggplot.type = "interaction",
+  nb_parameters_per_plot = 10
+){
+  if( attributes(out_mean_comparisons_predict_the_past_model_2)$PPBstats.object != "mean_comparisons_predict_the_past_model_2" ) { stop("data must come from mean_comparisons_predict_the_past_model_2") }
+  
+  attributes(out_mean_comparisons_predict_the_past_model_2)$PPBstats.object == "mean_comparisons_model_1"
+  
+  out = ggplot_mean_comparisons_model_1(out_mean_comparisons_predict_the_past_model_2, data_version, ggplot.type, nb_parameters_per_plot)
+  
+  return(out)
+}
+  
