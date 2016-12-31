c98e6625e71caeb645799ade22681f25858e1b73
priviere <pierre@semencespaysannes.org>
2016-12-31 14:10:24 +0100

    delete old code and replace with new one

--- R/predict_the_past_model_2.R
+++ R/predict_the_past_model_2.R
@@ -23,52 +23,33 @@
 #' @seealso \code{\link{FWH}}, \code{\link{analyse.outputs}}
 #' 
 #' 
-predict.the.past = function(
-  out.analyse.outputs,
-  output.format = "summary"
+predict_the_past_model_2 = function(
+  out_check_model_model_2
 )
   # let's go !!! ----------
 {
   # 1. Error message ----------  
-  if( !is.element(output.format, c("summary", "raw")) ) {  stop("output.format must be either \"summary\" or \"raw\".") }
+  if( attributes(out_check_model_model_2)$PPBstats.object != "check_model_model_2") {  stop("out_check_model_model_2 must come from check_model and model_2.") }
   
-  mess = "out.analyse.outputs must come from PPBstats::analyse.outputs."
-  MCMC = out.analyse.outputs$MCMC
-  if(is.null(MCMC)) { stop(mess) }
-  if(is.null(attributes(MCMC)$model)) { mess }
-    
-  # 3. Get the estimation based on MCMC outputs ----------
+  # 2. Get the estimation of mu_ij based on MCMC outputs ----------
+  w = out_check_model_model_2$model2.presence.abscence.matrix
+  MCMC = out_check_model_model_2$MCMC
   
-  w = out.analyse.outputs$model2.presence.abscence.matrix
-  if(is.null(w)) { stop(paste(mess, "Argument analysis in analyse.outputs must be NULL or \"experimental.design\".")) }
-  
-  pb <- txtProgressBar(min = 0, max = ncol(w), style = 3)
-  
-  if( output.format == "summary") { OUT = c(1:7) } else { OUT = c(1, nrow(MCMC)) }
-  n = NULL
-  
-  for(j in 1:ncol(w)) { 
-    env = colnames(w)[j]
-    germ = rownames(w)[which(w[,j] == 0)]
-    
-    if(length(germ) > 0) { # if length(geno) == 0, it means no germplasms must be estimated
-      for (i in 1:length(germ)) {
-        if (is.element(paste("alpha","[",germ[i],"]",sep=""), colnames(MCMC)) & is.element(paste("theta","[",env,"]",sep=""), colnames(MCMC)))  {
-          estimated.value = MCMC[,paste("alpha[",germ[i],"]",sep="")] + MCMC[,paste("beta[",germ[i],"]",sep="")] * MCMC[,paste("theta[",env,"]",sep="")]
-          if( output.format == "summary") { estimated.value = quantile(estimated.value, probs=c(0, 0.05, 0.10, 0.50, 0.90, 0.95, 1)) }
-          
-          OUT = rbind.data.frame(OUT, estimated.value)
-          n = c(n, paste("[", germ[i], ",", env,"]", sep = ""))
-        } else { warning("Estimated value for germplasm ", germ, " in environment ", env, " is not possible. This is because the estimation of germplasm and or environment effects did not converge and therefore were not in the MCMC.") 
-        }
+  j = 1
+  env = colnames(w)[j]
+  germ = rownames(w)[which(w[,j] == 0)]
+  if(length(germ) > 0) { # if length(geno) == 0, it means no germplasms must be estimated
+    pb <- txtProgressBar(min = 0, max = length(germ), style = 3)
+    for (i in 1:length(germ)) {
+      if (is.element(paste("alpha","[",germ[i],"]",sep=""), colnames(MCMC)) & is.element(paste("theta","[",env,"]",sep=""), colnames(MCMC)))  {
+        mu_estimated = MCMC[,paste("alpha[",germ[i],"]",sep="")] + MCMC[,paste("beta[",germ[i],"]",sep="")] * MCMC[,paste("theta[",env,"]",sep="")]
+        OUT = rbind.data.frame(OUT, mu_estimated)
+      } else { warning("Estimated value for germplasm ", germ, " in environment ", env, " is not possible. This is because the estimation of germplasm and or environment effects did not converge and therefore were not in the MCMC.") 
       }
+      setTxtProgressBar(pb, j)
     }
-    setTxtProgressBar(pb, j)
-  }
-  
-  OUT = OUT[-1,]
-  colnames(OUT) = c("0", "0.05", "0.10", "0.50", "0.90", "0.95", "1")
-  OUT$parameter = n
+    names(OUT) = paste("[", germ, ",", env,"]", sep = "")
+  } else { OUT = NULL }
   
   attributes(OUT)$PPBstats.object = "predict.the.past"  
   return(OUT)
