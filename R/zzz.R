2d1e77a4ae3aaba23f1893aa7d49f33b713b8fca
priviere <pierre@semencespaysannes.org>
2017-01-01 14:31:53 +0100

    update welcome message

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 # to print welcome message when loading the package
 .onLoad <- function(...) {
 packageStartupMessage("
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!! Thanks for using PPBstats :-)
-!!! For a tutorial, type vignette(\"PPBstats\")
-!!! To cite PPBstats, type citation(\"PPBstats\")
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!! Thanks for using PPBstats :-)                 !!!
+!!! For a tutorial, type vignette(\"PPBstats\")   !!!
+!!! To cite PPBstats, type citation(\"PPBstats\") !!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 "
 )
 }
