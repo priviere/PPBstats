06b290c5938be7c8bc0047c30abd18c48282ae09
priviere <pierre@semencespaysannes.org>
2017-01-14 14:22:55 +0100

    update Rd

--- R/get_ggplot.R
+++ R/get_ggplot.R
@@ -1,64 +1,77 @@
 # 0. help ----------
-#' Get ggplot objects to visualize output from the analysis
+#' Get ggplot objects to visualize output from several functions
 #'
 #' @description
-#' \code{get.ggplot} returns ggplot objects to visualize outputs from the analysis
+#' \code{get_ggplot} returns ggplot objects to visualize outputs from several functions
 #'
-#' @param data The data to plot. It can come from \code{get.mean.comparison}, \code{get.parameter.groups}, \code{predict.the.past}, \code{MC}$data_env_with_no_controls or \code{analye.outputs$model1.data_env_whose_param_did_not_converge}.
-#' 
-#' @param data_2 Outputs from \code{get.mean.comparisons} from model 2.For ggplot.type = "biplot-alpha-beta"
-#' 
-#' @param data_version data set with the following columns: "year", "location", "germplasm", "group", "version". The group refers to an id that contains two different versions. For example for group 1, there is version 1 and 2. See data(data_version) for an example.
-#'
-#' @param ggplot.type The type of plot you wish:
+#' @param data Outputs from 
 #' \itemize{
-#'  \item from \code{get.mean.comparison}
+#'  \item \code{check_model} or
+#'  \item \code{mean_comparisons} or
+#'  \item \code{parameter_groups} or
+#'  \item \code{biplot_GxE} or
+#'  \item \code{cross_validation_model_2} or
+#'  \item \code{predict_the_past_model_2}
+#' }
+#' 
+#' @param data_2 Outputs from \code{mean_comparisons} from \code{check_model} from \code{model_2}.
+#' This argument is used only when argument data comes from \code{mean_comparisons} from \code{check_model} from \code{model_2}.
+#' 
+#' @param data_version data set with the following columns: "year", "germplasm", "location", "group", "version". The group refers to an id that contains two different versions. For example for group 1, there is version 1 and 2. See data(data_version) for an example.
+#' This argument is used only for \code{mean_comparisons} from 
+#' \itemize{
+#'  \item \code{check_model} regarding \code{model_1}
+#'  \item \code{check_model} regarding \code{predict_the_past_model_2}
+#' }
+#'
+#' @param ggplot.type The type of plot you wish.
+#' \itemize{
+#'  \item "barplot" for mean_comparisons from 
 #'  \itemize{
-#'    \item from model 1 (\code{MC}) :  "barplot", "interaction", "score"
-#'    \item from model 2 (\code{FWH}): "barplot", "biplot-alpha-beta"
-#'    }
-#'      
-#'  \item from \code{get.parameter.groups} only from model 2 (\code{FWH}): "PCA"
-#'  
-#'  \item from \code{predict.the.past}$predict.the.past : "barplot", "interaction"
-#'  
-#'  \item from \code{MC}$data_env_with_no_controls : "barplot", "interaction"
-#'  
-#'  \item from \code{analye.outputs$model1.data_env_whose_param_did_not_converge} : "barplot", "interaction"
-#'  
+#'   \item check_model from GxE, model_1, model_2
+#'   \item predict_the_past_model_2
+#'   }
+#'  \item "interaction.plot" for mean_comparisons from
+#'  \itemize{
+#'   \item check_model from model_1
+#'   \item predict_the_past_model_2
+#'   }
+#'  \item "score" for mean_comparisons from
+#'  \itemize{
+#'   \item check_model from model_1
+#'   \item predict_the_past_model_2
+#'   }
+#'  \item "biplot-alpha-beta" for mean_comparisons from check_model from model_2
 #'  }
+#' 
+#' There are automatic plots generated for several functions:
+#' \itemize{
+#'  \item \code{biplot_GxE}
+#'  \item \code{check_model}
+#'  \item \code{parameter_groups}
+#'  \item \code{cross_validation_model_2}
+#'  }
+#' See details for more information.
 #' 
 #' @param nb_parameters_per_plot The number of parameters per plot to facilitate the visualization
 #' 
 #' @details
-#' \itemize{
-#' \item From \code{get.mean.comparison} and from model 1 (\code{MC}) there are one plot per environment.
-#' For ggplot.type = "interaction" and ggplot.type = "score", nb_parameters_per_plot is the number of entries (mu_ij).
-#' This is meaningful if you have data for several years.
 #' 
-#' ggplot.type = "score" display a plot with a score according to which group the entry was allocated.
-#' An high score means that the entry was in a group with an high mean.
-#' A low score means that the entry was in a group with an low mean.
-
-#' \item From \code{get.parameter.groups} from model 2 (\code{FWH}) "PCA" display the PCA and the groups of parameters.
-#' 
-#' On each plot, the alpha value and the alpha correction are displayed.
-#' alpha = Imp means that no differences were possible to find.
-#' For ggplot.type = "interaction" and ggplot.type = "score", it is display under the form: alpha | alpha correction
-#' 
-#' \item ggplot.type = "biplot-alpha-beta" display the biplot with alpha_i on the x axis and beta_i on the y axis.
-#' 
-#' \item When using data_version, and ggplot.type = "barplot"; the pvalue is computed based on the MCMC.
-#' For data that did not converge or without environments, it is a \code{t.test} which is perform.
-#' }
 #' 
 #' @return 
-#' The function returns a list of ggplot objects
+#'  
 #'  
 #' @author Pierre Riviere
 #' 
-#' @seealso \code{\link{get.mean.comparisons}}, \code{\link{get.parameter.groups}}
-#' 
+#' @seealso
+#' \itemize{
+#'  \item \code{check_model}
+#'  \item \code{mean_comparisons}
+#'  \item \code{parameter_groups}
+#'  \item \code{biplot_GxE}
+#'  \item \code{cross_validation_model_2}
+#'  \item \code{predict_the_past_model_2}
+#' }
 #' 
 get.ggplot = function(
   data,
