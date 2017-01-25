5f8fe168bada946774cc39d0a985095047e6daaf
priviere <pierre@semencespaysannes.org>
2017-01-25 21:08:19 +0100

    update plot and output name

--- R/ggplot_cross_validation_model_2.R
+++ R/ggplot_cross_validation_model_2.R
@@ -3,17 +3,17 @@ ggplot_cross_validation_model_2 = function(
 ){
   
   # Get the regression ----------
-  p = ggplot(out_cross_validation_model_2, aes(x = real.value, y = estimated.value)) 
-  p = p + stat_smooth(method = "lm") + geom_point() + xlab("observed value")
-  model = lm(real.value ~ estimated.value, data = out_cross_validation_model_2)
+  p = ggplot(out_cross_validation_model_2, aes(x = observed.value, y = estimated.value)) 
+  p = p + stat_smooth(method = "lm") + geom_point()
+  model = lm(observed.value ~ estimated.value, data = out_cross_validation_model_2)
   
   # Get the confidence in the estimation ----------
-  bias = out_cross_validation_model_2$real.value - out_cross_validation_model_2$estimated.value
+  bias = out_cross_validation_model_2$observed.value - out_cross_validation_model_2$estimated.value
   test = t.test(bias, mu = 0)
   proba_mean_equal_zero = round(test$p.value * 100, 1)
-  p = p + ggtitle("Cross validation", paste("Probability mean = 0: ", proba_mean_equal_zero))
+  p = p + ggtitle("Cross validation", paste("Probability mean = 0 : ", proba_mean_equal_zero))
   
-  out = list("plot" = p, "anova" = model)
+  out = list("plot" = p, "regression" = model)
   
   return(out)
 }
