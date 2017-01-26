008f2171816915651854707daba51a0b1777e00d
priviere <pierre@semencespaysannes.org>
2017-01-26 21:11:33 +0100

    update code to get same outptut as model 1 and model 2

--- R/ggplot_mean_comparisons_GxE.R
+++ R/ggplot_mean_comparisons_GxE.R
@@ -1,13 +1,13 @@
 ggplot_mean_comparisons_GxE = function(
   mean_comparisons_GxE,
-  ggplot.type,
+  ggplot.type = "barplot",
   nb_parameters_per_plot = 10
   ){
   
   # 1. Error message
   if( attributes(mean_comparisons_GxE)$PPBstats.object != "mean_comparisons_GxE" ) { stop("data must come from mean_comparisons and GxE") }
   
-  if( is.element(ggplot.type, c("barplot")) ) { stop("ggplot.type must be barplot with output from GxE.") }
+  if( !is.element(ggplot.type, c("barplot")) ) { stop("ggplot.type must be barplot with output from GxE.") }
   
   variable = mean_comparisons_GxE$variable
   
@@ -15,42 +15,49 @@ ggplot_mean_comparisons_GxE = function(
   data_ggplot_LSDbarplot_location = mean_comparisons_GxE$data_ggplot_LSDbarplot_location
   data_ggplot_LSDbarplot_year = mean_comparisons_GxE$data_ggplot_LSDbarplot_year
   
-  ggplot_LSDbarplot = function(d_LSD, fac, variable){
-    p = ggplot(data = d_LSD, aes(x = trt, y = means, label = M)) + geom_bar(stat = "identity")
-    p = p + geom_text(aes(y = ymean), angle = 90, color = "white")
-    p = p + theme(legend.position = "none", axis.text.x = element_text(angle = 90))
-    p = p + xlab(fac) + ylab(variable)
-    return(p)
+  ggplot_LSDbarplot = function(d_LSD, fac, variable, nb_parameters_per_plot){
+  
+    d_LSD = arrange(d_LSD, means) 
+    d_LSD$max = max(d_LSD$means, na.rm = TRUE)
+    d_LSD$split = add_split_col(d_LSD, nb_parameters_per_plot)
+    d_LSD_split = plyr:::splitter_d(d_LSD, .(split))  
+    
+    out = lapply(d_LSD_split, function(dx){
+      p = ggplot(d_LSD, aes(x = reorder(parameter, means), y = means)) + geom_bar(stat = "identity")
+      p = p + geom_text(aes(x = reorder(parameter, means), y = means/2, label = groups), angle = 90, color = "white")
+      p = p + ggtitle(paste(fac, "\n alpha = ", d_LSD[1, "alpha"], "; alpha correction :", d_LSD[1, "alpha.correction"]))
+      p = p + xlab("") + theme(axis.text.x = element_text(angle = 90)) + ylim(0, d_LSD[1,"max"]) + ylab(variable)
+      return(p)
+    })
+    
+    out = list(out)
+    names(out) = fac
+    return(out)
   }
   
   # 2. Germplasm
   if( !is.null(data_ggplot_LSDbarplot_germplasm) ){ 
-    ggplot_LSDbarplot_germplasm = ggplot_LSDbarplot(data_ggplot_LSDbarplot_germplasm, "germplasm", variable) 
+    ggplot_LSDbarplot_germplasm = ggplot_LSDbarplot(data_ggplot_LSDbarplot_germplasm, "germplasm", variable, nb_parameters_per_plot) 
   } else {
     ggplot_LSDbarplot_germplasm = NULL
     }
   
   # 3. Location
   if( !is.null(data_ggplot_LSDbarplot_location) ){ 
-    ggplot_LSDbarplot_location = ggplot_LSDbarplot(data_ggplot_LSDbarplot_location, "location", variable) 
+    ggplot_LSDbarplot_location = ggplot_LSDbarplot(data_ggplot_LSDbarplot_location, "location", variable, nb_parameters_per_plot) 
   } else {
     ggplot_LSDbarplot_location = NULL
   }
   
   # 4. Year
   if( !is.null(data_ggplot_LSDbarplot_year) ){ 
-    ggplot_LSDbarplot_year = ggplot_LSDbarplot(data_ggplot_LSDbarplot_year, "year", variable) 
+    ggplot_LSDbarplot_year = ggplot_LSDbarplot(data_ggplot_LSDbarplot_year, "year", variable, nb_parameters_per_plot) 
   } else {
     ggplot_LSDbarplot_year = NULL
   }
   
   # 5. return results
-  out = list(
-    "germplasm" = ggplot_LSDbarplot_germplasm,
-    "location" = ggplot_LSDbarplot_location,
-    "year" = ggplot_LSDbarplot_year
-  )
-  
+  out = c(ggplot_LSDbarplot_germplasm, ggplot_LSDbarplot_location, ggplot_LSDbarplot_year)
   return(out)
 }
 
