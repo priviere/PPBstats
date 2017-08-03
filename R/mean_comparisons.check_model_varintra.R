cda7b76f2876a3889573c943fb6d7e6b86734c67
gaelleVF <gaelle.van.frank@gmail.com>
2017-08-03 18:31:36 +0200

    delete info on which env is missing

--- R/mean_comparisons.check_model_varintra.R
+++ R/mean_comparisons.check_model_varintra.R
@@ -38,18 +38,10 @@ mean_comparisons.check_model_variance_intra = function(
   }
   
   data_mean_comparisons = MCMC_par(MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups)
-  attributes(data_mean_comparisons)$PPBstats.object = "data_mean_comparisons"
-  
-  # 3. Format data_env_with_no_controls and data_env_whose_param_did_not_converge
-  # to do !!!
-  data_env_with_no_controls = NULL
-  data_env_whose_param_did_not_converge = NULL
   
   # 4. Return results
   out <- list(
-    "data_mean_comparisons" = data_mean_comparisons,
-    "data_env_with_no_controls" = data_env_with_no_controls,
-    "data_env_whose_param_did_not_converge" = data_env_whose_param_did_not_converge
+    "data_mean_comparisons" = data_mean_comparisons
   )
   
   class(out) <- c("PPBstats", "mean_comparisons_model_variance_intra")
