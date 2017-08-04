14df164292877a04d97846342cdd3c2a1ad0d945
gaelleVF <gaelle.van.frank@gmail.com>
2017-08-04 10:57:07 +0200

    correct bug when asking for "mu" comparison

--- R/mean_comparisons.check_model_varintra.R
+++ R/mean_comparisons.check_model_varintra.R
@@ -9,7 +9,6 @@ mean_comparisons.check_model_variance_intra = function(
   p.adj = "soft.bonf"
 ){
   
-  library(qdapRegex)
   # 1. Error message
 
   if(!is.element(parameter, c("mu", "sigma"))) { stop("With outputs from model 1, the parameters must be mu or sigma") }
@@ -19,8 +18,8 @@ mean_comparisons.check_model_variance_intra = function(
   # 2. Get square matrice with pvalue or vector with pvalue ----------
   MCMC_par = function(MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups){
     a = colnames(MCMC)[grep(paste("^", parameter, "\\[", sep = ""), colnames(MCMC))]
-    vec_env = unique(unlist(lapply(a,function(x){ex_between(x, right = "]", left=",")})))
-    vec_MCMC_par = lapply(vec_env, function(env, MCMC){ MCMC[grep(paste(",", env, "]", sep = ""), colnames(MCMC))] }, MCMC)
+    vec_env = unlist(lapply(a,function(x){b = strsplit(x,",")[[1]]; b=b[grep("loc",b)]; b=strsplit(b,":")[[1]][1]}))
+    vec_MCMC_par = lapply(vec_env, function(env, MCMC){ MCMC[grep(env, colnames(MCMC))] }, MCMC)
     out = lapply(vec_MCMC_par, get_mean_comparisons_and_Mpvalue, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups) 
     
     fun = function(out, para){
@@ -37,7 +36,7 @@ mean_comparisons.check_model_variance_intra = function(
     return(out)
   }
   
-  data_mean_comparisons = MCMC_par(MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups)
+  data_mean_comparisons = MCMC_par(MCMC, parameter, type, threshold, alpha, p.adj, precision, get.at.least.X.groups=2)
   
   # 4. Return results
   out <- list(
