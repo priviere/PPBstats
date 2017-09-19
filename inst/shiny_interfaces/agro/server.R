c5a572017abea62f4579103b56b8b417ed671d14
priviere <pierre@semencespaysannes.org>
2017-09-19 13:42:04 +0200

    reorganize and comment to implement the interface step by step

--- inst/shiny_interfaces/agro/server.R
+++ inst/shiny_interfaces/agro/server.R
@@ -3,41 +3,43 @@
 
 server <- function(input, output) {
   
-  # Design experiment ----------
-  out_design_experiment = reactive(
-    design_experiment(
-      expe.type = input$design_experiment_expe.type,
-      location = input$design_experiment_location,
-      year = input$design_experiment_year,
-      germplasm = input$design_experiment_germplasm,
-      controls = input$design_experiment_controls,
-      nb.controls.per.block = input$design_experiment_nb.controls.per.block,
-      nb.blocks = input$design_experiment_nb.blocks,
-      nb.cols = input$design_experiment_nb.cols,
-      return.format = input$design_experiment_return.format
-    )
-  ) 
-  
-  output$design_experiment_plot = renderPlot({out_design_experiment$design})
-  output$design_experiment_data = renderPlot({out_design_experiment$data.frame})
-  
-  # AMMI GGE ----------
-  
   data("data_GxE")
   data_GxE = format_data_PPBstats(data_GxE, type = "data_agro")
   
-  output$plot1 <- renderPlot({
+  # Design experiment ----------
+  # out_design_experiment = reactive(
+  #   design_experiment(
+  #     expe.type = input$design_experiment_expe.type,
+  #     location = input$design_experiment_location,
+  #     year = input$design_experiment_year,
+  #     germplasm = input$design_experiment_germplasm,
+  #     controls = input$design_experiment_controls,
+  #     nb.controls.per.block = input$design_experiment_nb.controls.per.block,
+  #     nb.blocks = input$design_experiment_nb.blocks,
+  #     nb.cols = input$design_experiment_nb.cols,
+  #     return.format = input$design_experiment_return.format
+  #   )
+  # ) 
+  # 
+  # output$design_experiment_plot = renderPlot({out_design_experiment$design})
+  # output$design_experiment_data = renderPlot({out_design_experiment$data.frame})
+  # 
+  # Describe data ----------
+  output$describe_data_plot <- renderPlot({
     describe_data(
       data = data_GxE, 
-      plot_type = input$plot_type, 
-      x_axis = input$x_axis,
-      in_col = input$in_col,
-      vec_variables = input$vec_variables, 
-      nb_parameters_per_plot_x_axis = input$nb_parameters_per_plot_x_axis,
-      nb_parameters_per_plot_in_col = input$nb_parameters_per_plot_in_col,
-      labels_on = input$labels_on,
-      labels_size = input$labels_size
+      plot_type = input$describe_data_plot_type, 
+      x_axis = input$describe_data_x_axis,
+      in_col = input$describe_data_in_col,
+      vec_variables = input$describe_data_vec_variables, 
+      nb_parameters_per_plot_x_axis = input$describe_data_nb_parameters_per_plot_x_axis,
+      nb_parameters_per_plot_in_col = input$describe_data_nb_parameters_per_plot_in_col,
+      labels_on = input$describe_data_labels_on,
+      labels_size = input$describe_data_labels_size
     )
   })
+  
+  # AMMI GGE ----------
+  # data_GxE = format_data_PPBstats(data_GxE, type = "data_agro")
 }
 
