2feea05bdf46d108d54a9f8cf5c5b75d4ff7fe7d
priviere <pierre@semencespaysannes.org>
2017-11-07 21:10:26 +0100

    format code with several functions

--- R/plot.data_network.R
+++ R/plot.data_network.R
@@ -8,9 +8,9 @@ plot.data_network = function(
   labels_size = 4, 
   organize_sl = TRUE
   ){
-  n = ggnetwork(net, arrow.gap = 0.005)
   
-  if( is_bipartite(net) ) { 
+  # functions used afterward
+  plot_network_bipart = function(net, labels_on, labels_size){
     n = ggnetwork(net, arrow.gap = 0)
     ntmp = n
     
@@ -59,91 +59,104 @@ plot.data_network = function(
     p = p + geom_edges(arrow = arrow(length = unit(0, "pt"), type = "closed"))
     p = p + theme_blank()
     
-  } else {
-    
-    if( organize_sl){
-      n = ggnetwork(net, arrow.gap = 0)
-      
-      a = n
-      a$names = n$vertex.names
-      
-      lapply(a$names, function(x){
-        if(length(unlist(strsplit(as.character(x), "_")))!=4){
-          stop("Seed_lots must be under the following format : GERMPLASM_LOCATION_YEAR_DIGIT")
-          }
-        }
-        )
-      
-      a$g = sapply(a$names, function(x){ unlist(strsplit(as.character(x), "_"))[1] })
-      a$p = sapply(a$names, function(x){ unlist(strsplit(as.character(x), "_"))[2] })
-      a$ye = sapply(a$names, function(x){ unlist(strsplit(as.character(x), "_"))[3] })
-      a$d = sapply(a$names, function(x){ unlist(strsplit(as.character(x), "_"))[4] })
-      a$gd = paste(a$g, a$d, sep = "_")
-      
-      # Create a grid where the seed_lots will be put
-      # X are the xaxis of the new grid, it is the year
-      # Y are the y axis of the new grid
-      
-      # Y according to location and germplasm for a given location
-      pgd = with(a, table(p, gd))
-      vec_p = rownames(pgd)
-      
-      Y = person = germplasm_digit = person_limit = NULL
-      for(per in vec_p){
-        d = droplevels(a[which(a$p == per),])
-        ygd = with(d, table(ye, gd))
-        y = NULL
-        for(j in 1:ncol(ygd)){ y = c(y, max(ygd[,j])) }
-        y = c(y, 4) # make a gap between each location 
-        person_limit = c(person_limit, length(y) ) # and store the information for the plot after (in order to draw horizontal line)
-        Y = c(Y, y)
-        person = c(person, rep(per, length(y)))
-        germplasm_digit = c(germplasm_digit, colnames(ygd), "limit")
-      }
-      person_limit = cumsum(person_limit)
-      names(person_limit) = vec_p
-      Y = cumsum(Y)
-      dY = data.frame(person, germplasm_digit, Y)
-      
-      # Place seed_lots on the grid
-      # new coordinates
-      a$x_new = a$y_new = a$xend_new = a$yend_new = NA
-      
-      for(i in 1:nrow(a)) {
-        germ_digit = a[i, "gd"]
-        pers = a[i, "p"]
-        year = a[i, "ye"]
-        
-        x = a[i, "x"]
-        y = a[i, "y"]
-        xend = a[i, "xend"]
-        yend = a[i, "yend"]
-        
-        a[i, "x_new"] = year
-        a[i, "y_new"] = dY[which(dY$person == pers & dY$germplasm_digit == germ_digit), "Y"][1]
-      }
-      
-      x = a$x_new
-      names(x) = a$x
-      x = x[!duplicated(names(x))]
-      for(i in 1:length(x)) { a$xend_new[which(a$xend == names(x)[i])] = x[i] }
-      
-      y = a$y_new
-      names(y) = a$y
-      y = y[!duplicated(names(y))]
-      for(i in 1:length(y)) { a$yend_new[which(a$yend == names(y)[i])] = y[i] }
-      
-      # update n with new coordinates
-      n$x = a$x_new
-      n$y = a$y_new
-      n$xend = a$xend_new
-      n$yend = a$yend_new
+    if( labels_on ){ 
+      p = p + geom_nodelabel_repel(aes(label = vertex.names), size = labels_size, 
+                                   segment.color = "black") 
     }
     
-    if( is.null(in_col) ) { in_col = "location" }
-    if( organize_sl){ in_col = "germplasm"} 
-    colnames(n)[which(colnames(n) == in_col)] = "in_col" 
+    return(p)
+  }
+  plot_barplot_bipart = function(net){
+    s = sapply(V(net)$name, function(x) length(E(net)[from(V(net)[x])]))
+    s = s[which(vertex.attributes(net)$type == "germplasm")]
+    d = data.frame(germplasm = names(s), nb_location = s)
+    p = ggplot(d, aes(x = reorder(germplasm, -nb_location), y = nb_location)) + geom_bar(stat="identity")
+    p = p + xlab("germplasm") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    return(p)
+  }
+  organize_sl_unipart = function(net){
+    n = ggnetwork(net, arrow.gap = 0)
     
+    a = n
+    a$names = n$vertex.names
+    
+    lapply(a$names, function(x){
+      if(length(unlist(strsplit(as.character(x), "_")))!=4){
+        stop("Seed_lots must be under the following format : GERMPLASM_LOCATION_YEAR_DIGIT")
+      }
+    }
+    )
+    
+    a$g = sapply(a$names, function(x){ unlist(strsplit(as.character(x), "_"))[1] })
+    a$p = sapply(a$names, function(x){ unlist(strsplit(as.character(x), "_"))[2] })
+    a$ye = sapply(a$names, function(x){ unlist(strsplit(as.character(x), "_"))[3] })
+    a$d = sapply(a$names, function(x){ unlist(strsplit(as.character(x), "_"))[4] })
+    a$gd = paste(a$g, a$d, sep = "_")
+    
+    # Create a grid where the seed_lots will be put
+    # X are the xaxis of the new grid, it is the year
+    # Y are the y axis of the new grid
+    
+    # Y according to location and germplasm for a given location
+    pgd = with(a, table(p, gd))
+    vec_p = rownames(pgd)
+    
+    Y = person = germplasm_digit = person_limit = NULL
+    for(per in vec_p){
+      d = droplevels(a[which(a$p == per),])
+      ygd = with(d, table(ye, gd))
+      y = NULL
+      for(j in 1:ncol(ygd)){ y = c(y, max(ygd[,j])) }
+      y = c(y, 4) # make a gap between each location 
+      person_limit = c(person_limit, length(y) ) # and store the information for the plot after (in order to draw horizontal line)
+      Y = c(Y, y)
+      person = c(person, rep(per, length(y)))
+      germplasm_digit = c(germplasm_digit, colnames(ygd), "limit")
+    }
+    person_limit = cumsum(person_limit)
+    Y = cumsum(Y)
+    person_limit = Y[person_limit]; names(person_limit) = vec_p
+    dY = data.frame(person, germplasm_digit, Y)
+    
+    # Place seed_lots on the grid
+    # new coordinates
+    a$x_new = a$y_new = a$xend_new = a$yend_new = NA
+    
+    for(i in 1:nrow(a)) {
+      germ_digit = a[i, "gd"]
+      pers = a[i, "p"]
+      year = a[i, "ye"]
+      
+      x = a[i, "x"]
+      y = a[i, "y"]
+      xend = a[i, "xend"]
+      yend = a[i, "yend"]
+      
+      a[i, "x_new"] = year
+      a[i, "y_new"] = dY[which(dY$person == pers & dY$germplasm_digit == germ_digit), "Y"][1]
+    }
+    
+    x = a$x_new
+    names(x) = a$x
+    x = x[!duplicated(names(x))]
+    for(i in 1:length(x)) { a$xend_new[which(a$xend == names(x)[i])] = x[i] }
+    
+    y = a$y_new
+    names(y) = a$y
+    y = y[!duplicated(names(y))]
+    for(i in 1:length(y)) { a$yend_new[which(a$yend == names(y)[i])] = y[i] }
+    
+    # update n with new coordinates
+    n$x = a$x_new
+    n$y = a$y_new
+    n$xend = a$xend_new
+    n$yend = a$yend_new
+    
+    out = list("person_limit" = person_limit, "n" = n)
+    
+    return(out)
+  }
+  plot_network_unipart = function(n){
     nr = n[which(n$relation_type != "diffusion"),]
     nd = n[which(n$relation_type == "diffusion"),]
     
@@ -154,44 +167,66 @@ plot.data_network = function(
     p$labels$colour = in_col
     
     scale_ex = c("solid", "dotted", "longdash", "dashed", "twodash", "dotdash")
-    p + scale_linetype_manual(values = scale_ex[1:length(na.omit(unique(n$relation_type)))] )
-    
-    if( organize_sl){
-      
-      r = range(as.numeric(as.character(n$x)))
-      m = max(as.numeric(as.character(n$x)))
-      m = m + (r[2]-r[1]) /length(r[1]:r[2])
-      
-      d_lab = data.frame(x = as.character(rep(m, length(person_limit))),
-                     y = tapply(n$y, n$location, function(x){mean(range(x))}),
-                     location = c(names(person_limit))
-                     )
-      
-      p = p + geom_hline(yintercept = c(0, Y[person_limit])) 
-      p = p + geom_label(data = d_lab, aes(x = x, y = y, label = location))
-      p = p + theme(axis.title.y = element_blank(),
-                axis.text.y = element_blank(),
-                axis.ticks.y = element_blank(),
-                axis.ticks.x = element_blank(),
-                panel.background = element_blank()
-                )
-      
-      p = p + xlab("year") + scale_x_discrete(labels = c(r[1]:r[2], ""))
-      
-    } else {
-      p = p + theme_blank()
-      }
-    
+    p = p + scale_linetype_manual(values = scale_ex[1:length(na.omit(unique(n$relation_type)))] )
+    return(p)
   }
-  
-  if( labels_on ){ 
-    p = p + geom_nodelabel_repel(
-      aes(label = vertex.names), 
-      size = labels_size,
-      segment.color = "black"
-    ) 
+  plot_network_organize_sl_unipart = function(n, person_limit){
+    p = plot_network_unipart(n)
+    r = range(as.numeric(as.character(n$x)))
+    m = max(as.numeric(as.character(n$x)))
+    m = m + (r[2]-r[1]) /length(r[1]:r[2])
+    
+    d_lab = data.frame(x = as.character(rep(m, length(person_limit))),
+                       y = tapply(n$y, n$location, function(x){mean(range(x))}),
+                       location = c(names(person_limit))
+    )
+    
+    p = p + geom_hline(yintercept = c(0, person_limit)) 
+    p = p + geom_label(data = d_lab, aes(x = x, y = y, label = location), inherit.aes = FALSE)
+    p = p + theme(axis.title.y = element_blank(),
+                  axis.text.y = element_blank(),
+                  axis.ticks.y = element_blank(),
+                  axis.ticks.x = element_blank(),
+                  panel.background = element_blank()
+    )
+    
+    p = p + xlab("year") + scale_x_discrete(labels = c(r[1]:r[2], ""))
+    return(p)
   }
   
   
-  return(p)
+  if( is_bipartite(net) ) { 
+    out = list(
+      "network" = plot_network_bipart(net, labels_on, labels_size), 
+      "barplot" = plot_barplot_bipart(net)
+      )
+  } else {
+    
+    if( organize_sl){ 
+      out = organize_sl_unipart(net) 
+      person_limit = out$person_limit
+      n = out$n
+    } else { 
+        n = ggnetwork(net, arrow.gap = 0.005) 
+        }
+    
+    if( is.null(in_col) ) { in_col = "location" }
+    if( organize_sl){ in_col = "germplasm"} 
+    colnames(n)[which(colnames(n) == in_col)] = "in_col" 
+    
+    if( organize_sl){ 
+      p = plot_network_organize_sl_unipart(n, person_limit) 
+    } else { 
+        p = plot_network_unipart(n) + theme_blank() 
+        }
+    
+    if( labels_on ){ 
+      p = p + geom_nodelabel_repel(aes(label = vertex.names), size = labels_size, 
+                                   segment.color = "black") 
+    }
+    
+    out = list("network" = p)
+  }
+  
+  return(out)
 }
