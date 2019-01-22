b382541639210b30de92a6ed7c12a3d0af736902
Baptiste Rouger <baptiste.rouger@cri-paris.org>
2019-01-22 14:13:09 +0100

    Create mean_comp...odel_home_away on the same shape as local foreign

--- R/mean_comparisons.check_model_home_away.R
+++ R/mean_comparisons.check_model_home_away.R
@@ -1,9 +1,9 @@
-#' Get mean comparisons from \code{\link{check_model.fit_model_local_foreign}} object
+#' Get mean comparisons from \code{\link{check_model.fit_model_home_away}} object
 #'
 #' @description
-#' \code{mean_comparisons} performs mean comparisons from object coming from \code{\link{check_model.fit_model_local_foreign}}
+#' \code{mean_comparisons} performs mean comparisons from object coming from \code{\link{check_model.fit_model_home_away}}
 #'
-#' @param x outputs from \code{\link{check_model.fit_model_local_foreign}}
+#' @param x outputs from \code{\link{check_model.fit_model_home_away}}
 #'
 #' @param alpha level of type one error. 0.05 (5\%) by default
 #'
@@ -21,8 +21,8 @@
 #' @return
 #'  A list of four elements :
 #'   \itemize{
-#'    \item info : a list with variable and local_foreign analysis
-#'    \item data_ggplot_LSDbarplot_version:location
+#'    \item info : a list with variable and home_away analysis
+#'    \item data_ggplot_LSDbarplot_version:germplasm
 #'    \item data_ggplot_LSDbarplot_germplasm
 #'    \item data_ggplot_LSDbarplot_location
 #'    \item data_ggplot_LSDbarplot_year
@@ -34,22 +34,21 @@
 #' \itemize{
 #'  \item \code{\link{mean_comparisons}}
 #'  \item \code{\link{plot.PPBstats}}
-#'  \item \code{\link{plot.mean_comparisons_model_local_foreign}}
+#'  \item \code{\link{plot.mean_comparisons_model_home_away}}
 #' }
 #'
 #' @export
 #'
 #' @import emmeans
-#'
-mean_comparisons.check_model_local_foreign <- function(
+mean_comparisons.check_model_home_away <- function(
   x,
   alpha = 0.05,
   p.adj = "none",
   ...
 ){
   # get info
-  model = x$model_local_foreign$ANOVA$model
-  variable = x$model_local_foreign$info$variable
+  model = x$model_home_away$ANOVA$model
+  variable = x$model_home_away$info$variable
 
   # run LSmeans
   if(length(grep("year", attr(model$terms,"term.labels"))) == 0){
@@ -57,7 +56,7 @@ mean_comparisons.check_model_local_foreign <- function(
                    "germplasm" = emmeans::emmeans(model,"germplasm"),
                    "version" = emmeans::emmeans(model,"version"),
                    # "block:location" = emmeans::emmeans(model, pairwise~block|location),
-                   "version:location" = emmeans::emmeans(model, pairwise~version|location)
+                   "version:germplasm" = emmeans::emmeans(model, pairwise~version|germplasm)
     )
     fac_single = c("location", "germplasm")
 
@@ -68,20 +67,20 @@ mean_comparisons.check_model_local_foreign <- function(
                    "version" = emmeans::emmeans(model,"version"),
                    # "block:location" = emmeans::emmeans(model, pairwise~block|location:year),
                    "location:year" = emmeans::emmeans(model, pairwise~year|location),
-                   "version:location" = emmeans::emmeans(model, pairwise~version|location),
-                   "version:location:year" = emmeans::emmeans(model, pairwise~version|year:location)
+                   "version:germplasm" = emmeans::emmeans(model, pairwise~version|germplasm),
+                   "version:germplasm:year" = emmeans::emmeans(model, pairwise~version|year:germplasm)
     )
     fac_single = c("location", "germplasm", "year")
   }
 
   # mean comparisons
   # ftg = grep(":", names(LSmeans))
-  fac_inter = "version:location" # names(LSmeans)[ftg]
+  fac_inter <- "version:germplasm" # names(LSmeans)[ftg]
 
-  out_fac_inter = lapply(LSmeans[fac_inter], function(x){
-    out_pairs = as.data.frame(summary(x$emmeans))
+  out_fac_inter <- lapply(LSmeans[fac_inter], function(x){
+    out_pairs <- as.data.frame(summary(x$emmeans))
 
-    out_stars = as.data.frame(summary(x$contrasts))
+    out_stars <- as.data.frame(summary(x$contrasts))
     stars = sapply(out_stars$p.value, function(pvalue){
       if(is.null(pvalue)) { stars = " "} else {
         if(pvalue < 0.001) { stars = "***" }
@@ -91,11 +90,11 @@ mean_comparisons.check_model_local_foreign <- function(
       }
       return(stars)
     })
-    names(stars) = out_stars[,2]
+    names(stars) <- out_stars[,2]
 
-    out_pairs$stars = stars[out_pairs[,2]]
+    out_pairs$stars <- stars[out_pairs[,2]]
 
-    out_pairs = data.frame("parameter" = out_pairs[,2],
+    out_pairs <- data.frame("parameter" = out_pairs[,2],
                            "version" = out_pairs[,1],
                            "means" = out_pairs[,"emmean"],
                            "stars" = out_pairs[,"stars"],
@@ -104,12 +103,12 @@ mean_comparisons.check_model_local_foreign <- function(
     return(out_pairs)
   }
   )
-  names(out_fac_inter) = paste("data_ggplot_LSDbarplot_", fac_inter, sep = "")
+  names(out_fac_inter) <- paste("data_ggplot_LSDbarplot_", fac_inter, sep = "")
 
 
-  out_fac_single = lapply(LSmeans[fac_single], function(x, alpha, p.adj) {
-    out_cld = emmeans::CLD(x, alpha = alpha, Letters = letters, adjust = p.adj)
-    out_cld = data.frame("parameter" = out_cld[,1],
+  out_fac_single <- lapply(LSmeans[fac_single], function(x, alpha, p.adj) {
+    out_cld <- emmeans::CLD(x, alpha = alpha, Letters = letters, adjust = p.adj)
+    out_cld <- data.frame("parameter" = out_cld[,1],
                          "means" = out_cld[,2],
                          "groups" = out_cld[,7],
                          "alpha" = alpha,
@@ -117,9 +116,9 @@ mean_comparisons.check_model_local_foreign <- function(
     return(out_cld)
     }, alpha = alpha, p.adj = p.adj
   )
-  names(out_fac_single) = paste("data_ggplot_LSDbarplot_", fac_single, sep = "")
+  names(out_fac_single) <- paste("data_ggplot_LSDbarplot_", fac_single, sep = "")
 
-  out = c(list("info" = x$model_local_foreign$info), c(out_fac_inter, out_fac_single))
-  class(out) <- c("PPBstats", "mean_comparisons_model_local_foreign")
+  out <- c(list("info" = x$model_home_away$info), c(out_fac_inter, out_fac_single))
+  class(out) <- c("PPBstats", "mean_comparisons_model_home_away")
   return(out)
 }
